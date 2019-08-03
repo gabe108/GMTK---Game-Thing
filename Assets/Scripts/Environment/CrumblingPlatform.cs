7a2a68f3964018cffb2ab3d080d83b5d7dc15b9d
Muhammad Ghalib <mg.gabe108@gmail.com>
2019-08-03 23:13:51 +1000

    Moving platform and crubling platforms done

---
+++ Assets/Scripts/Environment/CrumblingPlatform.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrumblingPlatform : MonoBehaviour
+{
+	[Range(0,1)]
+	[SerializeField] private float m_offset;
+	private List<Transform> m_crumbles = new List<Transform>();
+
+	private void Start()
+	{
+		for(int i = 0; i < this.transform.childCount; i++)
+		{
+			m_crumbles.Add(this.transform.GetChild(i));
+		}
+		BoxCollider2D collider = m_crumbles[0].gameObject.GetComponent<BoxCollider2D>();
+		int width = (int)(collider.bounds.extents.x * 2);
+		for(int i = 0; i < m_crumbles.Count; i++)
+		{
+			Vector3 temp = Vector3.zero;
+			temp.x = (width + m_offset) * i;
+			m_crumbles[i].position += temp;
+		}
+	}
+}
---
+++ Assets/Scripts/Environment/Hazards/BaseHazard.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseHazard : MonoBehaviour
+{
+	[Range(0, 1)]
+	[SerializeField] protected float m_delay;
+
+	protected float m_timer;
+	protected bool m_startTimer;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public virtual void Evaluate()
+	{
+
+	}
+}
