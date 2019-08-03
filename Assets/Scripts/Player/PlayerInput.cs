7e98a3f14c9224e781c1305d3351eab14f2b781f
Dillon Handyside <dj_handyside@hotmail.com>
2019-08-03 12:34:41 +1000

    Implemented Player Move (Left+Right, A+D)

---
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    [SerializeField] private PlayerMovement m_movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (m_movement == null)
+            Debug.LogWarning("PlayerInput: PlayerMovement not assigned!");
+
+        
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    void Update()
+    {
+        m_movement.Move(Input.GetAxisRaw("Horizontal"));
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float m_speed = 1.0f;
+
+    private CharacterController m_characterController;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private void Start()
+    {
+        m_characterController = GetComponent<CharacterController>();
+    }
+
+    /// <summary>
+    /// use this function to perform player movement!
+    /// </summary>
+    /// <param name="xAxis">value between -1 and 1 (left and right)</param>
+    public void Move(float xAxis)
+    {
+        Vector3 direction = new Vector3(xAxis, 0, 0);
+
+        // perform movement
+        m_characterController.Move(direction * m_speed * Time.deltaTime);
+    }
+}
