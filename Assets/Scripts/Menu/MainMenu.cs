2e7d20c27b834674ba7d7d0699d0b4e9260b3c8a
Muhammad Ghalib <mg.gabe108@gmail.com>
2019-08-03 14:07:45 +1000

    Menus Done

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject m_mainMenu;
+    [SerializeField] private GameObject m_settingsMenu;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Menu()
+    {
+        m_mainMenu.SetActive(true);
+        m_settingsMenu.SetActive(false);
+    }
+
+    public void Settings()
+    {
+        m_mainMenu.SetActive(false);
+        m_settingsMenu.SetActive(true);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
