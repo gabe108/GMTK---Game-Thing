c2531a2c9501b2982501dcb14e48435511d127e6
Dillon Handyside <dj_handyside@hotmail.com>
2019-08-04 13:03:55 +1000

    Implemented Darkness

---
+++ Assets/Scripts/Level/Darkness.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Darkness : MonoBehaviour
+{
+    [Header("Positional Variables")]
+    [SerializeField] private Vector3 m_startOffset;
+    [SerializeField] private Vector3 m_direction = Vector3.right;
+
+    [Header("Speeds")]
+    [SerializeField] private float m_defaultSpeed = 1;
+    [SerializeField] private float m_flagSpeed = 0.5f;
+
+    private void Update()
+    {
+        transform.position += m_direction * m_defaultSpeed * Time.deltaTime;
+    }
+
+    public void ResetDarkness(Transform playerPosition)
+    {
+        transform.position = playerPosition.position + m_startOffset;
+    }
+}
